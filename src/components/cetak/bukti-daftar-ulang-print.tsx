f2f706cf14176a77e19ccd797fea25183090e581
Akhmad Lutfi Disini <alfahroe@gmail.com>
2025-05-03 04:35:49 +0000

    sudah lebih baik baik tinggal atur tingginya

--- src/components/cetak/bukti-daftar-ulang-print.tsx
+++ src/components/cetak/bukti-daftar-ulang-print.tsx
@@ -82,7 +82,7 @@ const Receipt: React.FC<{ data: BuktiDaftarUlangData; isArsip?: boolean; namaPet
                 <div className="w-10 h-10 bg-transparent flex items-center justify-center mr-2 flex-shrink-0 print:w-10 print:h-10 print:mr-2" data-ai-hint="school logo green">
                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-10 h-10 text-green-700 print:w-10 print:h-10"> {/* Matched size */}
                         <path d="M11.25 4.533A9.707 9.707 0 0 0 6 3a9.735 9.735 0 0 0-3.25.555.75.75 0 0 0-.5.707v14.522c0 .318.218.594.5.707A9.735 9.735 0 0 0 6 21a9.707 9.707 0 0 0 5.25-1.533v-1.42a.75.75 0 0 0-.657-.744A8.202 8.202 0 0 1 6 18a8.235 8.235 0 0 1-2.25-.37v-1.42a.75.75 0 0 1 .657-.744A8.21 8.21 0 0 0 6 15c2.086 0 3.981-.782 5.378-2.067a.75.75 0 0 0 1.122-.56v-1.42a.75.75 0 0 0-.5-.707 8.21 8.21 0 0 0-1.721-.486.75.75 0 0 0-.657.744v1.42h-.001c-1.431.925-3.312 1.483-5.323 1.483a8.235 8.235 0 0 1-2.25-.37V7.5a8.21 8.21 0 0 0 1.721-.486.75.75 0 0 1 .657.744v1.42c0 .274.11.523.294.706A8.21 8.21 0 0 0 6 10.5c2.086 0 3.981-.782 5.378-2.067a.75.75 0 0 1 1.122-.56v-1.42a.75.75 0 0 1 .5-.707c.157-.054.316-.1.477-.143a.75.75 0 0 0 .6-.89Z" />
-                        <path d="M12.75 3a9.735 9.735 0 0 1 3.25.555.75.75 0 0 1 .5.707v14.522c0 .318-.218.594-.5.707A9.735 9.735 0 0 1 12.75 21a9.707 9.707 0 0 1-5.25-1.533v-1.42a.75.75 0 0 1 .657-.744 8.202 8.202 0 0 0 4.593-.345 8.235 8.235 0 0 0 2.25-.37v-1.42a.75.75 0 0 0-.657-.744 8.21 8.21 0 0 1-4.593-.345c-2.086 0-3.981.782-5.378 2.067a.75.75 0 0 1-1.122.56v1.42a.75.75 0 0 1 .5.707 8.21 8.21 0 0 1 1.721.486.75.75 0 0 1 .657-.744v-1.42h.001c1.431-.925 3.312-1.483 5.323-1.483a8.235 8.235 0 0 0 2.25.37V13.5a8.21 8.21 0 0 1-1.721.486.75.75 0 0 0-.657.744v-1.42a.75.75 0 0 1-.294-.706 8.21 8.21 0 0 1-1.622-4.533c2.086 0 3.981.782 5.378 2.067a.75.75 0 0 0 1.122.56v1.42a.75.75 0 0 0 .5-.707c.157.054.316.1.477.143a.75.75 0 0 1 .6.89Z" />
+                        <path d="M12.75 3a9.735 9.735 0 0 1 3.25.555.75.75 0 0 1 .5.707v14.522c0 .318-.218.594-.5.707A9.735 9.735 0 0 1 12.75 21a9.707 9.707 0 0 1-5.25-1.533v-1.42a.75.75 0 0 1 .657-.744 8.202 8.202 0 0 0 4.593-.345 8.235 8.235 0 0 0 2.25-.37v-1.42a.75.75 0 0 0-.657-.744 8.21 8.21 0 0 1-4.593-.345c-2.086 0-3.981.782-5.378 2.067a.75.75 0 0 1-1.122.56v1.42a.75.75 0 0 1 .5.707 8.21 8.21 0 0 1 1.721.486.75.75 0 0 1 .657.744v-1.42h.001c1.431-.925 3.312-1.483 5.323-1.483a8.235 8.235 0 0 0 2.25.37V13.5a8.21 8.21 0 0 1-1.721.486.75.75 0 0 0-.657.744v-1.42a.75.75 0 0 1-.294-.706 8.21 8.21 0 0 1-1.622-4.533c2.086 0 3.981.782 5.378 2.067a.75.75 0 0 0 1.122.56v1.42a.75.75 0 0 0 .5-.707c.157.054.316.1.477-.143a.75.75 0 0 1 .6.89Z" />
                     </svg>
                 </div>
                 {/* Text Alignment & Styling */}
@@ -91,7 +91,7 @@ const Receipt: React.FC<{ data: BuktiDaftarUlangData; isArsip?: boolean; namaPet
                     <p className="text-sm font-bold text-green-700 print:text-[10pt] mb-0">MA NU 01 BANYUPUTIH</p> {/* Larger school name */}
                     <p className="text-xs font-bold print:text-[9pt] mb-0">TAHUN PELAJARAN 2025 / 2026</p> {/* Added slash */}
                     {/* Divider */}
-                    <div className="h-[2px] bg-teal-600 my-0.5 print:h-[1.5pt] print:my-0.5"></div>
+                    <div className="h-[2px] bg-red-600 my-0.5 print:h-[1.5pt] print:my-0.5"></div>
                 </div>
            </div>
         </div>
@@ -131,21 +131,21 @@ const Receipt: React.FC<{ data: BuktiDaftarUlangData; isArsip?: boolean; namaPet
                          </span>
                       )}
                 </div>
-            </div>
-             {/* Info Box - Positioned directly below the checklist */}
-             <div className={cn(
-                 "border border-black p-1 text-center mt-2 mb-1 text-[8pt] leading-tight print:text-[7pt] print:my-1 print:p-1 small-print bg-gray-100 print:bg-gray-100" // Added mt-2
-              )}>
-                 {!isArsip ? (
-                     <>
-                         Selamat bergabung di Madrasah Hebat, MA NU 01 Banyuputih.<br/>
-                         Info keberangkatan pertama akan di informasikan di grup Whatsapp Siswa Baru 2025
-                     </>
-                 ) : (
-                     <>&nbsp;</> // Add non-breaking space to maintain height if border collapses
-                 )}
              </div>
-         </div>
+              {/* Info Box - Positioned directly below the checklist */}
+              <div className={cn(
+                  "border border-black p-1 text-center mt-2 mb-1 text-[8pt] leading-tight print:text-[7pt] print:my-1 print:p-1 small-print bg-gray-100 print:bg-gray-100" // Added mt-2
+               )}>
+                  {!isArsip ? (
+                      <>
+                          Selamat bergabung di Madrasah Hebat, MA NU 01 Banyuputih.<br/>
+                          Info keberangkatan pertama akan di informasikan di grup Whatsapp Siswa Baru 2025
+                      </>
+                  ) : (
+                      <>&nbsp;</> // Add non-breaking space to maintain height if border collapses
+                  )}
+              </div>
+          </div>
 
         {/* Footer - Pushes to bottom */}
         <div className="receipt-footer mt-auto"> {/* Use mt-auto to push */}
